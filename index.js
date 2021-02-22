621674d5a493ba3212c01781cde8faa9932b37ce
voldfix <myai.ivan@gmail.com>
2021-02-22 19:13:49 +0300

    feat(crud-api): crud-operations as get post delete is added, for post add conditional check for empty body and fn for generate unique Id is created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const express = require('express')
 
 let notes = [
 	{
@@ -18,11 +18,66 @@ let notes = [
 	}
 ]
 
-const app = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'application/json' })
-	res.end(JSON.stringify(notes))
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+	res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+	const id = req.params.id
+	const note = notes.find(n => n.id === id)
+	
+	if (note) {
+		res.json(note)
+	} else {
+		res.statusMessage = `resource at url: /api/notes/:${id} not exist`
+		res.status(404).end()
+	}
+})
+
+const idGenerator = () => {
+	const maxId = notes.length > 0
+		? Math.max(...notes.map(n => Number(n.id)))
+		: 0
+	
+	return String(maxId + 1)
+}
+
+app.post('/api/notes', (req, res) => {
+	const body = req.body
+
+	if (!body.content) {
+		res.status(400).json({
+			error: 'content missing'
+		})
+	}
+
+	const note = {
+		content: body.content,
+		important: body.important || false,
+		id: idGenerator()
+	}
+
+	notes = notes.concat(note)
+
+	console.log(note)
+	res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+	const id = req.params.id
+	notes = notes.filter(n => n.id !== id)
+
+	res.status(204).end()
 })
 
 const PORT = 3009
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
