0664981081296ea1f27318c83ebf7d40efcf48b4
voldfix <myai.ivan@gmail.com>
2021-03-02 22:52:54 +0300

    fix(port) enviroment varible is added for deploy

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.delete('/api/notes/:id', (req, res) => {
 	res.status(204).end()
 })
 
-const PORT = 3009
+const PORT = process.env.PORT || 3009
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
